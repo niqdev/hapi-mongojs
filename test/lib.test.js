c4c8607839795cea67b0d3f14947db726fe9eadb
niqdev <niqdev@gmail.com>
2016-03-31 17:51:59 +0200

    skeleton test

---
+++ test/lib.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const Code = require('code');
+const Lab = require('lab');
+
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+
+describe('test hapi-mongojs', () => {
+
+  it('should verify XXX', (done) => {
+    expect(true).to.equal(true);
+    done();
+  });
+
+});
